0d1f37523f58721300b84a2b0cee0a6ace2fcf23
jodi.a.schneider <jodi.a.schneider@gmail.com>
2007-07-06 10:15:24 +0000

    Skeleton for citation export

--- dspace/branches/2007/citations/dspace/src/org/dspace/citations/ExportToRIS.java
+++ dspace/branches/2007/citations/dspace/src/org/dspace/citations/ExportToRIS.java
@@ -14,35 +14,35 @@ public class ExportToRIS {
 	 * @param args outputfilename handle1 handle2 ...
 	 * @throws SQLException 
 	 */
-	 
-	 /** log4j category */
- 
-	 
+
+	/** log4j category */
+
+
 	public static void main(String[] args) throws SQLException {
-		
+
 		//private static Logger log = Logger.getLogger(ExportToRIS.class); 
-		
+
 		//get outfilename
 		String outfilename = args[0];
-		
-        //open file for reading
+
+		//open file for reading
 		try
 		{
 			PrintWriter outfile = new PrintWriter (
 					new FileOutputStream(outfilename));
-	
-		//do something with handles
-		//Context context = new Context();
-		
-		//get handles and process them
-		for (int i = 1; i < args.length; i++){
-			System.out.print("Processing " + args[i]+"...");
-			outfile.print(args[i]);
-			outfile.println();	
-		}   
-		//end the line
-		System.out.println();
-		
+
+			//do something with handles
+			//Context context = new Context();
+
+			//get handles and process them
+			for (int i = 1; i < args.length; i++){
+				System.out.print("Processing " + args[i]+"...");
+				outfile.print(args[i]);
+				outfile.println();	
+			}   
+			//end the line
+			System.out.println();
+
 			outfile.close();
 		}
 		catch (IOException e)
