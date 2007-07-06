b0672690e0dea907ae7dd430f09914671c957606
jodi.a.schneider <jodi.a.schneider@gmail.com>
2007-07-06 10:10:54 +0000

    Skeleton for citation export

---
+++ dspace/branches/2007/citations/dspace/src/org/dspace/citations/ExportToRIS.java
@@ -0,0 +1,54 @@
+package org.dspace.citations;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+//import org.apache.log4j.Logger;
+//import org.dspace.core.*;
+
+public class ExportToRIS {
+
+	/**
+	 * @param args outputfilename handle1 handle2 ...
+	 * @throws SQLException 
+	 */
+	 
+	 /** log4j category */
+ 
+	 
+	public static void main(String[] args) throws SQLException {
+		
+		//private static Logger log = Logger.getLogger(ExportToRIS.class); 
+		
+		//get outfilename
+		String outfilename = args[0];
+		
+        //open file for reading
+		try
+		{
+			PrintWriter outfile = new PrintWriter (
+					new FileOutputStream(outfilename));
+	
+		//do something with handles
+		//Context context = new Context();
+		
+		//get handles and process them
+		int i = 1; while (i < args.length){
+			System.out.print("Processing " + args[i]+"...");
+			outfile.print(args[i]);
+			outfile.println();
+			i++;			
+		}   
+		//end the line
+		System.out.println();
+		
+			outfile.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
