2aab9754906c16a9d2151373999866fce046628b
jodi.a.schneider <jodi.a.schneider@f843d996-dd2e-0410-98de-9970085ac1de>
2007-07-08 06:42:34 +0000

    Untested version of ExportToRIS.java Policy failures: Code warning - failed on resource TESTMEExportToRIS.java. Reason: The local variable styled is never read, line 7 Override reason: Test version

--- dspace/branches/2007/citations/dspace/src/org/dspace/citations/TESTMEExportToRIS.java
+++ dspace/branches/2007/citations/dspace/src/org/dspace/citations/TESTMEExportToRIS.java
@@ -3,30 +3,48 @@ package org.dspace.citations;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.Iterator;
 import java.util.List;
 
 //import org.apache.log4j.Logger;
-import org.dspace.content.DSpaceObject;
+import org.dspace.content.Item;
 import org.dspace.core.Constants;
 import org.dspace.core.Context;
+import org.dspace.core.PluginManager;
 import org.dspace.handle.HandleManager;
+import org.dspace.content.crosswalk.CrosswalkInternalException;
+import org.dspace.content.crosswalk.XSLTDisseminationCrosswalk;
+import org.dspace.content.packager.PackageDisseminator;
+
+import org.jdom.Document;
+//import org.jdom.output.XMLOutputter;
+import org.jdom.transform.XSLTransformer;
+
 
 public class TESTMEExportToRIS {
 
 	/**
 	 * @param args outputfilename handle1 handle2 ...
-	 * @throws SQLException 
+	 * @throws Exception 
 	 */
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws Exception {
 		/** log4j category */
 		//Logger log = Logger.getLogger(TESTMEExportToRIS.class); //private static gives me an error--huh?
-		//TODO: use log.warn and write to log
+		//TODO: use log.warn and write to log		
+		
+		XSLTDisseminationCrosswalk myXSLTDisseminationCrosswalk = new XSLTDisseminationCrosswalk(); 
 
+		String CROSSWALK_PLUGIN = "echom"; 			//pass-through stylesheet, echoes metadata
+		String STYLESHEET = "DIM2RIS.xsl";
+		PackageDisseminator dip = (PackageDisseminator)
+          PluginManager.getNamedPlugin(PackageDisseminator.class, CROSSWALK_PLUGIN);
+				
+		if (dip == null)
+            throw new CrosswalkInternalException("Cannot find a disseminate plugin for package=" + CROSSWALK_PLUGIN);
 		//get outfilename
 		String outfilename = args[0];
+
+		System.setProperty( "javax.xml.transform.TransformerFactory", "org.apache.xalan.processor.TransformerFactoryImpl");
 
 		//open file for reading
 		try
@@ -34,41 +52,42 @@ public class TESTMEExportToRIS {
 			PrintWriter outfile = new PrintWriter (
 					new FileOutputStream(outfilename));
 
+			Item myItem = null;
 			Context context = new Context();
+			context.setIgnoreAuthorization(true);
 
 			//get handles and process them
 			for (int i = 1; i < args.length; i++){
-				String suspectedhandle = args[i]; //Expected user to input handles
-				System.out.print("Processing " + suspectedhandle +"...");
-				outfile.print(suspectedhandle);
-				outfile.println();
-				//NOTE: changed HandleManager.getHandlesForPrefix visibility to public
-				List handleslist = 
-					HandleManager.getHandlesForPrefix(context, suspectedhandle); 
-				if (handleslist.isEmpty())
-					System.out.print("No handle found starting with " 
-							+ suspectedhandle + ".\n");
-				else if (!handleslist.contains(suspectedhandle)){
-					System.out.print("Sorry" + suspectedhandle + 
-							"was not found. The handles starting with" 
-							+ suspectedhandle + "are: ");
-					for(Iterator j = handleslist.iterator(); j.hasNext(); )
-						System.out.println(j.next());
-					System.out.println();
+				String myIDString = args[i]; //Expected user to input handles
+				System.out.print("Processing " + myIDString +"...");
+
+				// first, is myIDString a handle?
+				if (myIDString.indexOf('/') != -1)
+				{
+					myItem = (Item) HandleManager.resolveToObject(context, myIDString);
+					outfile.print(myIDString + "/n");
+					List dimlist = myXSLTDisseminationCrosswalk.disseminateList(myItem);
+					XSLTransformer newstylesheet = new XSLTransformer(STYLESHEET);
+					Document styled = (Document) newstylesheet.transform(dimlist); 
+					// Output the document
+					
+					if ((myItem == null) || (myItem.getType() != Constants.ITEM))
+					{
+						myItem = null;
+					}
 				}
-				else //suspectedhandle is a real handle; do real work here
-					System.out.print("Found handle" + suspectedhandle + "\n");
-				DSpaceObject dso = 
-					HandleManager.resolveToObject(context, suspectedhandle);
-				if (dso.getType() != Constants.ITEM)
-			          System.out.print("Export to RIS can only handle Items.");
-				//TODO: do something with mydso here
-				//We know it's an Item.
-				//
-				
+				else
+				{
+					myItem = Item.find(context, Integer.parseInt(myIDString));
+				}
 
+				if (myItem == null)
+				{
+					System.out
+					.println("Error, item cannot be found: " + myIDString);
+				}
 			}   
-
+			
 			outfile.close();
 		}
 		catch (IOException e)
