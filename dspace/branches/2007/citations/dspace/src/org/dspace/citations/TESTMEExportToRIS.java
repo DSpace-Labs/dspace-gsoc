b68d00766f014eb421704f36fa729471d2cfcfef
jodi.a.schneider <jodi.a.schneider@gmail.com>
2007-07-09 13:36:21 +0000

    Configures XSLTDisseminationCrosswalk via PluginManager to use passthrough stylesheet echom.xsl (must be configured in dspace.cfg) to get DIM. Transforms DIM by STYLESHEET (e.g. DIM2RIS.xsl). Outputs result to file (and intermediate results to screen for now).

--- dspace/branches/2007/citations/dspace/src/org/dspace/citations/TESTMEExportToRIS.java
+++ dspace/branches/2007/citations/dspace/src/org/dspace/citations/TESTMEExportToRIS.java
@@ -3,7 +3,7 @@ package org.dspace.citations;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
+import java.util.*;
 
 //import org.apache.log4j.Logger;
 import org.dspace.content.Item;
@@ -11,13 +11,12 @@ import org.dspace.core.Constants;
 import org.dspace.core.Context;
 import org.dspace.core.PluginManager;
 import org.dspace.handle.HandleManager;
-import org.dspace.content.crosswalk.CrosswalkInternalException;
-import org.dspace.content.crosswalk.XSLTDisseminationCrosswalk;
-import org.dspace.content.packager.PackageDisseminator;
+import org.dspace.content.crosswalk.*;
 
-import org.jdom.Document;
-//import org.jdom.output.XMLOutputter;
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
 import org.jdom.transform.XSLTransformer;
+
 
 
 public class TESTMEExportToRIS {
@@ -31,68 +30,79 @@ public class TESTMEExportToRIS {
 		/** log4j category */
 		//Logger log = Logger.getLogger(TESTMEExportToRIS.class); //private static gives me an error--huh?
 		//TODO: use log.warn and write to log		
-		
-		XSLTDisseminationCrosswalk myXSLTDisseminationCrosswalk = new XSLTDisseminationCrosswalk(); 
 
+		//initialize Crosswalk 
 		String CROSSWALK_PLUGIN = "echom"; 			//pass-through stylesheet, echoes metadata
-		String STYLESHEET = "DIM2RIS.xsl";
-		PackageDisseminator dip = (PackageDisseminator)
-          PluginManager.getNamedPlugin(PackageDisseminator.class, CROSSWALK_PLUGIN);
-				
+		String STYLESHEET = "DIM2RIS.xsl";	        //put in /bin directory; starts with DIM
+		DisseminationCrosswalk dip = (DisseminationCrosswalk)PluginManager.getNamedPlugin(org.dspace.content.crosswalk.XSLTDisseminationCrosswalk.class, "echom");
+
+		//Self-named plugin of CROSSWALK_PLUGIN must be initialized with PluginManager		
 		if (dip == null)
-            throw new CrosswalkInternalException("Cannot find a disseminate plugin for package=" + CROSSWALK_PLUGIN);
-		//get outfilename
-		String outfilename = args[0];
+		{
+			throw new CrosswalkInternalException("Cannot find a disseminate plugin for package=" + CROSSWALK_PLUGIN);
+		}
 
 		System.setProperty( "javax.xml.transform.TransformerFactory", "org.apache.xalan.processor.TransformerFactoryImpl");
 
-		//open file for reading
-		try
-		{
-			PrintWriter outfile = new PrintWriter (
-					new FileOutputStream(outfilename));
+		//		get outfilename
+		String outfilename = args[0];
 
-			Item myItem = null;
-			Context context = new Context();
-			context.setIgnoreAuthorization(true);
+		Item myItem = null;
+		Context context = new Context();
+		context.setIgnoreAuthorization(true);
 
-			//get handles and process them
-			for (int i = 1; i < args.length; i++){
-				String myIDString = args[i]; //Expected user to input handles
-				System.out.print("Processing " + myIDString +"...");
+		//get Item ID's and process them
+		for (int i = 1; i < args.length; i++){
+			String myIDString = args[i]; //Expected user to input handles
 
-				// first, is myIDString a handle?
-				if (myIDString.indexOf('/') != -1)
+			// first, is myIDString a handle?
+			if (myIDString.indexOf('/') != -1)
+			{
+				//TODO: Prevent Cast Error Exception on (myItem.getType() != Constants.ITEM))
+				myItem = (Item) HandleManager.resolveToObject(context, myIDString); 
+
+				if ((myItem == null) || (myItem.getType() != Constants.ITEM))
 				{
-					myItem = (Item) HandleManager.resolveToObject(context, myIDString);
-					outfile.print(myIDString + "/n");
-					List dimlist = myXSLTDisseminationCrosswalk.disseminateList(myItem);
-					XSLTransformer newstylesheet = new XSLTransformer(STYLESHEET);
-					Document styled = (Document) newstylesheet.transform(dimlist); 
-					// Output the document
-					
-					if ((myItem == null) || (myItem.getType() != Constants.ITEM))
-					{
-						myItem = null;
+					myItem = null;
+				}
+			}
+			else 
+			{
+				myItem = Item.find(context, Integer.parseInt(myIDString));
+			}
+
+			if (myItem == null)
+			{
+				System.out
+				.println("Error, item cannot be found: " + myIDString);}
+			else
+			{
+				try
+				{
+					List dimlist = dip.disseminateList(myItem);
+					XMLOutputter x = new XMLOutputter();
+					Iterator it = dimlist.iterator();
+					while (it.hasNext()) {
+						System.out.println(x.outputString((Element)it.next()));
 					}
-				}
-				else
-				{
-					myItem = Item.find(context, Integer.parseInt(myIDString));
-				}
+					
+					//transform and print to screen
+					XSLTransformer newstylesheet = new XSLTransformer(STYLESHEET);
+					List styled = newstylesheet.transform(dimlist); 					
+					System.out.println(styled.toString()); //for testing 
 
-				if (myItem == null)
-				{
-					System.out
-					.println("Error, item cannot be found: " + myIDString);
+					//print styled output
+					PrintWriter outfile = 
+						new PrintWriter (new FileOutputStream(outfilename));
+					x.output(styled, outfile);
+					outfile.flush();
+					outfile.close();
 				}
-			}   
-			
-			outfile.close();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
