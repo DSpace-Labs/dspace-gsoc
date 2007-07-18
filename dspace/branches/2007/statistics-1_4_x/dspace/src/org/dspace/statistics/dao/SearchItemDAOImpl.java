1377cd653cf66b80af0bed9e817fffd3421d1a91
federico.paparoni <federico.paparoni@f843d996-dd2e-0410-98de-9970085ac1de>
2007-07-18 22:27:35 +0000

    First implementation of SearchItem with DB save/update

--- dspace/branches/2007/statistics-1_4_x/dspace/src/org/dspace/statistics/dao/SearchItemDAOImpl.java
+++ dspace/branches/2007/statistics-1_4_x/dspace/src/org/dspace/statistics/dao/SearchItemDAOImpl.java
@@ -3,14 +3,10 @@ package org.dspace.statistics.dao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.StringTokenizer;
 
 import org.apache.log4j.Logger;
 
-import org.dspace.content.Item;
-import org.dspace.core.ConfigurationManager;
 import org.dspace.core.Context;
-import org.dspace.core.LogManager;
 import org.dspace.storage.rdbms.DatabaseManager;
 import org.dspace.storage.rdbms.TableRow;
 import org.dspace.storage.rdbms.TableRowIterator;
@@ -26,23 +22,37 @@ public class SearchItemDAOImpl implements SearchItemDAO{
 	// TODO Fix this query with LIKE or create another method for it
 	private String findByQuery = "SELECT * FROM search_stats WHERE query = ?";
 	private String delete = "DELETE FROM search_stats WHERE search_stats_id = ?";
-
+	private ArrayList columns;
 
 	public SearchItemDAOImpl(Context context) {
 		this.context=context;
+		this.columns = new ArrayList();
+		columns.add("search_stats_id");
+		columns.add("query");
+		columns.add("count");
+		columns.add("last_modified");
 	}
 
 	public boolean commit(SearchItem si) throws SearchItemException {
 		TableRow row;
 		try {
-			row = DatabaseManager.create(context, "search_stats");
-			log.info("Dice "+row.getIntColumn("search_stats_id"));
-			//row.setColumn("search_stats_id", row.getIntColumn("search_stats_id"));
-	        row.setColumn("last_modified", new Date());
-	        row.setColumn("query", si.getQuery());
-	        int res=DatabaseManager.update(context, row);
-
-	        log.info("Dice dopo"+res);
+			SearchItem results[]=findByQuery(si.getQuery());
+			if (results==null) {
+				row = DatabaseManager.create(context, "search_stats");
+				row.setColumn("last_modified", new Date());
+		        row.setColumn("query", si.getQuery());
+		        row.setColumn("count", 1);
+		        int res=DatabaseManager.update(context, row);
+			}
+			else {
+				SearchItem saved_si=results[0];
+		        TableRow tr = new TableRow("search_stats",columns);
+		        tr.setColumn("search_stats_id", saved_si.getID());
+		        tr.setColumn("query", saved_si.getQuery());
+		        tr.setColumn("count", saved_si.getCount()+1);
+		        tr.setColumn("last_modified", new Date());
+		        DatabaseManager.update(context, tr);
+			}
 			return true;
 		} catch (SQLException e) {
 			throw new SearchItemException(e);
@@ -79,7 +89,42 @@ public class SearchItemDAOImpl implements SearchItemDAO{
 	}
 
 	public SearchItem[] findByQuery(String query) throws SearchItemException {
-		return null;
+		try
+        {
+	        ArrayList mappings = new ArrayList();
+	        Object[] params = { query };
+	        TableRowIterator tri = DatabaseManager.query(context, findByQuery, params);
+	        int count=0;
+	        int id=0;
+	        Date timestamp;
+
+	        if (!tri.hasNext())
+	        {
+	                return null;
+	        }
+	        else {
+	        	TableRow tr = tri.next();
+	        	count=tr.getIntColumn("count");
+	        	id=tr.getIntColumn("search_stats_id");
+	        	timestamp=tr.getDateColumn("last_modified");
+
+	        	SearchItem si=create();
+	        	si.setID(id);
+	        	si.setCount(count);
+	        	si.setQuery(query);
+	        	si.setTimestamp(timestamp);
+	        	mappings.add(si);
+	        }
+	        tri.close();
+
+	        SearchItem[] maps = new SearchItem[mappings.size()];
+	        return (SearchItem[]) mappings.toArray((SearchItem[]) maps);
+        }
+        catch (SQLException e)
+        {
+                log.error("caught exception: ", e);
+                throw new SearchItemException(e);
+        }
 	}
 
 	public SearchItem findBySearchItem(SearchItem searchItem) throws SearchItemException {
