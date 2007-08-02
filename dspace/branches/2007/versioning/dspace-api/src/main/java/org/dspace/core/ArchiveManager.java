df5366852b7996778b1d82146ef1882d83d2fcc8
Mark Diggory <mdiggory@users.noreply.github.com>
2007-08-02 04:01:12 +0000

    r6333@libaxis1 (orig r316):  jimsaku | 2007-08-01 10:52:33 -0400  ResourcePolicy DAOs. They seem to be working fine. Not 100% convinced they are being deleted properly, but that could be an issue elsewhere

--- dspace/branches/2007/versioning/dspace-api/src/main/java/org/dspace/core/ArchiveManager.java
+++ dspace/branches/2007/versioning/dspace-api/src/main/java/org/dspace/core/ArchiveManager.java
@@ -412,17 +412,10 @@ public class ArchiveManager
             // currentUser because he started the removal process and he will
             // end it too. also add right to remove from the item to remove
             // it's bundles.
-            try
-            {
-                AuthorizeManager.addPolicy(context, item, Constants.DELETE,
-                        context.getCurrentUser());
-                AuthorizeManager.addPolicy(context, item, Constants.REMOVE,
-                        context.getCurrentUser());
-            }
-            catch (SQLException sqle)
-            {
-                throw new RuntimeException(sqle);
-            }
+            AuthorizeManager.addPolicy(context, item, Constants.DELETE,
+                    context.getCurrentUser());
+            AuthorizeManager.addPolicy(context, item, Constants.REMOVE,
+                    context.getCurrentUser());
 
             itemDAO.delete(item.getID());
         }
@@ -453,17 +446,10 @@ public class ArchiveManager
             // currentUser because he started the removal process and he will
             // end it too. also add right to remove from the child to
             // remove it's items.
-            try
-            {
-                AuthorizeManager.addPolicy(context, child, Constants.DELETE,
-                        context.getCurrentUser());
-                AuthorizeManager.addPolicy(context, child, Constants.REMOVE,
-                        context.getCurrentUser());
-            }
-            catch (SQLException sqle)
-            {
-                throw new RuntimeException(sqle);
-            }
+            AuthorizeManager.addPolicy(context, child, Constants.DELETE,
+                    context.getCurrentUser());
+            AuthorizeManager.addPolicy(context, child, Constants.REMOVE,
+                    context.getCurrentUser());
 
             // Orphan; delete it
             collectionDAO.delete(child.getID());
@@ -494,17 +480,10 @@ public class ArchiveManager
             // currentUser because he started the removal process and he will
             // end it too. also add right to remove from the collection to
             // remove it's items.
-            try
-            {
-                AuthorizeManager.addPolicy(context, child, Constants.DELETE,
-                        context.getCurrentUser());
-                AuthorizeManager.addPolicy(context, child, Constants.REMOVE,
-                        context.getCurrentUser());
-            }
-            catch (SQLException sqle)
-            {
-                throw new RuntimeException(sqle);
-            }
+            AuthorizeManager.addPolicy(context, child, Constants.DELETE,
+                    context.getCurrentUser());
+            AuthorizeManager.addPolicy(context, child, Constants.REMOVE,
+                    context.getCurrentUser());
 
             communityDAO.delete(child.getID());
         }
