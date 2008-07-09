3b988483b0177af3f9e789c36d50c52f4103e55c
major.error <major.error@f843d996-dd2e-0410-98de-9970085ac1de>
2008-07-09 13:25:46 +0000

    Added authorization to metadata system. Caches results of auth requests in Maps to ensure it doesn't hit database too hard

--- dspace/branches/2008/semanticweb/dspace-api/src/main/java/org/dspace/metadata/MetadataManager.java
+++ dspace/branches/2008/semanticweb/dspace-api/src/main/java/org/dspace/metadata/MetadataManager.java
@@ -1,29 +1,30 @@
 package org.dspace.metadata;
 
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.DSpaceObject;
 
 public interface MetadataManager
 {
     
-    public MetadataCollection getMetadata( DSpaceObject o );
+    public MetadataCollection getMetadata( DSpaceObject o ) throws AuthorizeException;
     
-    public MetadataCollection getMetadata( DSpaceObject o, int depth );
+    public MetadataCollection getMetadata( DSpaceObject o, int depth ) throws AuthorizeException;
     
-    public MetadataCollection getMetadata( Selector s );
+    public MetadataCollection getMetadata( Selector s ) throws AuthorizeException;
     
-    public MetadataCollection getMetadata( Selector s, int depth );
+    public MetadataCollection getMetadata( Selector s, int depth ) throws AuthorizeException;
     
-    public void addMetadata( MetadataItem ... is );
+    public void addMetadata( MetadataItem ... is ) throws AuthorizeException;
     
-    public void addMetadata( MetadataCollection coll );
+    public void addMetadata( MetadataCollection coll ) throws AuthorizeException;
     
-    public void addMetadata( DSpaceObject o, Predicate p, Value v );
+    public void addMetadata( DSpaceObject o, Predicate p, Value v ) throws AuthorizeException;
     
-    public void addMetadata( DSpaceObject o, Predicate p, DSpaceObject v );
+    public void addMetadata( DSpaceObject o, Predicate p, DSpaceObject v ) throws AuthorizeException;
     
-    public void removeMetadata( Selector s );
+    public void removeMetadata( Selector s ) throws AuthorizeException;
     
-    public void removeAllMetadata( DSpaceObject o );
+    public void removeAllMetadata( DSpaceObject o ) throws AuthorizeException;
     
     public void beginTransaction();
     
--- dspace/branches/2008/semanticweb/dspace-api/src/main/java/org/dspace/metadata/jena/MetadataManagerJena.java
+++ dspace/branches/2008/semanticweb/dspace-api/src/main/java/org/dspace/metadata/jena/MetadataManagerJena.java
@@ -3,10 +3,16 @@ package org.dspace.metadata.jena;
 import com.hp.hpl.jena.rdf.model.Statement;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import org.apache.log4j.Logger;
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.authorize.AuthorizeManager;
 import org.dspace.content.DSpaceObject;
+import org.dspace.core.Constants;
+import org.dspace.core.Context;
 import org.dspace.dao.jena.GlobalDAOJena;
 import org.dspace.metadata.MetadataCollection;
 import org.dspace.metadata.MetadataItem;
@@ -21,9 +27,15 @@ public class MetadataManagerJena implements MetadataManager
     private Logger log = Logger.getLogger( MetadataManagerJena.class );
     private GlobalDAOJena dao;
     private StatementTranslator tran;
+    private Context context;
+    private Map<DSpaceObject,Boolean> read, write, remove;
 
-    public MetadataManagerJena()
+    public MetadataManagerJena( Context c )
     {
+        context = c;
+        read = new HashMap<DSpaceObject,Boolean>();
+        write = new HashMap<DSpaceObject,Boolean>();
+        remove = new HashMap<DSpaceObject,Boolean>();
         try
         {
             dao = new GlobalDAOJena();
@@ -34,13 +46,15 @@ public class MetadataManagerJena implements MetadataManager
         }
     }
 
-    public MetadataCollection getMetadata( DSpaceObject o )
+    public MetadataCollection getMetadata( DSpaceObject o ) throws AuthorizeException
     {
+        auth( o, Constants.READ );
         return getMetadata( o, 1 );
     }
 
-    public MetadataCollection getMetadata( final DSpaceObject o, int depth )
+    public MetadataCollection getMetadata( final DSpaceObject o, int depth ) throws AuthorizeException
     {
+        auth( o, Constants.READ );
         return getMetadata( (Selector) new SelectorCore()
                     {
 
@@ -67,25 +81,31 @@ public class MetadataManagerJena implements MetadataManager
         while ( it.hasNext() )
         {
             MetadataItem m = it.next();
-            c.add( m );
-            if ( m.isDSpaceObject() && depth > 1 )
-            {
-                final DSpaceObject newSub = m.getDSpaceObject();
-                Iterator<MetadataItem> cit = getMetadata(
-                        (Selector) new SelectorCore( s )
-                        {
+            try {
+                auth( m.getSubject(), Constants.READ );
+                c.add( m );
+                if ( m.isDSpaceObject() && depth > 1 )
+                {
+                    final DSpaceObject newSub = m.getDSpaceObject();
+                    try {
+                        auth( newSub, Constants.READ );
+                        Iterator<MetadataItem> cit = getMetadata(
+                                (Selector) new SelectorCore( s )
+                                {
 
-                            @Override
-                            public DSpaceObject getSubject()
-                            {
-                                return newSub;
-                            }
+                                    @Override
+                                    public DSpaceObject getSubject()
+                                    {
+                                        return newSub;
+                                    }
 
-                        },
-                        depth - 1 ).getMetadata();
-                while ( cit.hasNext() )
-                    c.add( cit.next() );
-            }
+                                },
+                                depth - 1 ).getMetadata();
+                        while ( cit.hasNext() )
+                            c.add( cit.next() );
+                    } catch ( AuthorizeException e ) { }
+                }
+            } catch ( AuthorizeException e ) { }
         }
         return new MetadataCollectionJena( c );
     }
@@ -95,7 +115,13 @@ public class MetadataManagerJena implements MetadataManager
         try
         {
             beginTransaction();
-            dao.getTripleStore().add( tran.translate( is ) );
+            for ( MetadataItem m : is )
+            {
+                try {
+                    auth( m.getSubject(), Constants.WRITE );
+                    dao.getTripleStore().add( tran.translate( m ) );
+                } catch ( AuthorizeException e ) { }
+            }
             commitTransaction();
         } catch ( Exception e )
         {
@@ -122,17 +148,26 @@ public class MetadataManagerJena implements MetadataManager
         addMetadata( MetadataFactory.createItem( o, p, v ) );
     }
 
-    public void removeMetadata( Selector s )
+    public void removeMetadata( Selector s ) throws AuthorizeException
     {
-        if ( s.isValueMatcher() ) 
+        if ( s.isValueMatcher() && s.getSubject() != null ) 
         {
+            auth( s.getSubject(), Constants.REMOVE );
             Statement st = tran.translate( s );
-            dao.getTripleStore().removeAll( st.getSubject(), st.getPredicate(),
-                                            st.getObject() );
+            dao.getTripleStore().removeAll( st.getSubject(), 
+                        st.getPredicate(), st.getObject() );
         } else 
         {
-            dao.getTripleStore().remove( dao.getTripleStore().listStatements( 
-                    SelectorCore.toJenaSelector( s, tran ) ) );
+            Iterator<Statement> it = dao.getTripleStore().listStatements( 
+                    SelectorCore.toJenaSelector( s, tran ) );
+            while ( it.hasNext() )
+            {
+                Statement curr = it.next();
+                try {
+                    auth( tran.translate( curr ).getSubject(), Constants.REMOVE );
+                    dao.getTripleStore().remove( curr );
+                } catch ( AuthorizeException e ) { }
+            }
         }
     }
 
@@ -157,6 +192,44 @@ public class MetadataManagerJena implements MetadataManager
     {
         if ( dao.getTripleStore().supportsTransactions() )
             dao.getTripleStore().commit();
+    }
+
+    private Boolean auth( DSpaceObject subject, int action ) throws AuthorizeException
+    {
+        switch( action )
+        {
+            case Constants.READ : 
+                    if ( read.containsKey( subject ) && read.get( subject ) )
+                        return true;
+                    else
+                        throw new AuthorizeException();
+            case Constants.WRITE : 
+                    if ( write.containsKey( subject ) && write.get( subject ) )
+                        return true;
+                    else
+                        throw new AuthorizeException();
+            case Constants.REMOVE : 
+                    if ( remove.containsKey( subject ) && remove.get( subject ) )
+                        return true;
+                    else
+                        throw new AuthorizeException();
+            default : break;
+        }
+        boolean res = true;
+        try 
+        {
+            AuthorizeManager.authorizeAction( context, subject, action );
+        } catch ( AuthorizeException e )
+        {
+            res = false;
+        }
+        switch( action )
+        {
+            case Constants.READ : return read.put( subject, res );
+            case Constants.WRITE : return write.put( subject, res );
+            case Constants.REMOVE : return remove.put( subject, res );
+            default : return res;
+        }
     }
 
 }
