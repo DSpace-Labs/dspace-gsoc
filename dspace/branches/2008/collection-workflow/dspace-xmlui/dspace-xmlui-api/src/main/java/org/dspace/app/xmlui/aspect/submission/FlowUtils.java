a62b2770f88ef9d7acc8617b5bdb19c716e788f2
bram.deschouwer <bram.deschouwer@f843d996-dd2e-0410-98de-9970085ac1de>
2008-08-18 15:53:07 +0000

    Failed commit: Default

--- dspace/branches/2008/collection-workflow/dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/submission/FlowUtils.java
+++ dspace/branches/2008/collection-workflow/dspace-xmlui/dspace-xmlui-api/src/main/java/org/dspace/app/xmlui/aspect/submission/FlowUtils.java
@@ -68,8 +68,9 @@ import org.dspace.core.Context;
 import org.dspace.core.LogManager;
 import org.dspace.handle.HandleManager;
 import org.dspace.submit.AbstractProcessingStep;
-import org.dspace.workflow.WorkflowItem;
-import org.dspace.workflow.WorkflowManager;
+import org.dspace.workflow_new.WorkflowItem;
+import org.dspace.workflow_new.WorkflowManager;
+import org.dspace.workflow_new.WorkflowConfigurationException;
 
 /**
  * This is a utility class to aid in the submission flow scripts. 
@@ -100,8 +101,7 @@ public class FlowUtils {
 	 * @param inProgressSubmissionID
 	 * @return The InprogressSubmission or null if non found
 	 */
-	public static InProgressSubmission findSubmission(Context context, String inProgressSubmissionID) throws SQLException
-	{
+	public static InProgressSubmission findSubmission(Context context, String inProgressSubmissionID) throws SQLException, AuthorizeException, IOException, WorkflowConfigurationException {
 		char type = inProgressSubmissionID.charAt(0);
 		int id = Integer.valueOf(inProgressSubmissionID.substring(1));
 		
@@ -126,8 +126,7 @@ public class FlowUtils {
 	 * @param inProgressSubmissionID
 	 * @return The found workspaceitem or null if none found.
 	 */
-	public static WorkspaceItem findWorkspace(Context context, String inProgressSubmissionID) throws SQLException
-	{
+	public static WorkspaceItem findWorkspace(Context context, String inProgressSubmissionID) throws SQLException, AuthorizeException, IOException, WorkflowConfigurationException {
 		InProgressSubmission submission = findSubmission(context, inProgressSubmissionID);
 		if (submission instanceof WorkspaceItem)
 			return (WorkspaceItem) submission;
@@ -143,8 +142,7 @@ public class FlowUtils {
 	 * @param inProgressSubmissionID
 	 * @return The found workflowitem or null if none found.
 	 */
-	public static WorkflowItem findWorkflow(Context context, String inProgressSubmissionID) throws SQLException
-	{
+	public static WorkflowItem findWorkflow(Context context, String inProgressSubmissionID) throws SQLException, AuthorizeException, IOException, WorkflowConfigurationException {
 		InProgressSubmission submission = findSubmission(context, inProgressSubmissionID);
 		if (submission instanceof WorkflowItem)
 			return (WorkflowItem) submission;
@@ -163,8 +161,7 @@ public class FlowUtils {
      * 
      * @return a SubmissionInfo object
      */
-    public static SubmissionInfo obtainSubmissionInfo(Map objectModel, String workspaceID) throws SQLException
-    {
+    public static SubmissionInfo obtainSubmissionInfo(Map objectModel, String workspaceID) throws SQLException, AuthorizeException, IOException, WorkflowConfigurationException {
         Request request = ObjectModelHelper.getRequest(objectModel);
         Context context = ContextUtil.obtainContext(objectModel);
           
@@ -218,8 +215,7 @@ public class FlowUtils {
      * @param page the page (within the step) the user has just reached
      */
     public static void setPageReached(Context context, String id, int step, int page)
-            throws SQLException, AuthorizeException, IOException
-    {
+            throws SQLException, AuthorizeException, IOException, WorkflowConfigurationException {
         InProgressSubmission submission = findSubmission(context, id);
 		
 		if (submission instanceof WorkspaceItem)
@@ -250,7 +246,7 @@ public class FlowUtils {
      * @param context The current DSpace content
 	 * @param id The unique ID of the current workflow/workspace
      */
-	public static int getMaximumStepReached(Context context, String id) throws SQLException {
+	public static int getMaximumStepReached(Context context, String id) throws SQLException, AuthorizeException, IOException, WorkflowConfigurationException {
 		
 		InProgressSubmission submission = findSubmission(context, id);
 		
@@ -275,7 +271,7 @@ public class FlowUtils {
      * @param context The current DSpace content
 	 * @param id The unique ID of the current workflow/workspace
      */
-	public static int getMaximumPageReached(Context context, String id) throws SQLException {
+	public static int getMaximumPageReached(Context context, String id) throws SQLException, AuthorizeException, IOException, WorkflowConfigurationException {
 		
 		InProgressSubmission submission = findSubmission(context, id);
 		
@@ -334,8 +330,7 @@ public class FlowUtils {
 	 * @param id The unique ID of the current workspace/workflow
 	 * @param request The cocoon request object.
 	 */
-	public static void processSaveOrRemove(Context context, String id, Request request) throws SQLException, AuthorizeException, IOException
-	{
+    public static void processSaveOrRemove(Context context, String id, Request request) throws SQLException, AuthorizeException, IOException, WorkflowConfigurationException {
 		if (request.getParameter("submit_remove") != null)
 		{
 			// If they selected to remove the item then delete everything.
@@ -344,142 +339,6 @@ public class FlowUtils {
 	        context.commit();
 		}
 	}
-	
-	/**
-	 * Update the provided workflowItem to advance to the next workflow 
-	 * step. If this was the last thing needed before the item is 
-	 * committed to the repository then return true, otherwise false.
-	 * 
-	 * @param context The current DSpace content
-	 * @param id The unique ID of the current workflow
-	 */
-	public static boolean processApproveTask(Context context, String id) throws SQLException, UIException, ServletException, AuthorizeException, IOException
-	{
-		WorkflowItem workflowItem = findWorkflow(context, id);
-		Item item = workflowItem.getItem();
-		
-		// Advance the item along the workflow
-        WorkflowManager.advance(context, workflowItem, context.getCurrentUser());
-
-        // FIXME: This should be a return value from advance()
-        // See if that gave the item a Handle. If it did,
-        // the item made it into the archive, so we
-        // should display a suitable page.
-        String handle = HandleManager.findHandle(context, item);
-
-        context.commit();
-        
-        if (handle != null)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-	}
-	
-	
-	
-	/**
-	 * Return the given task back to the pool of unclaimed tasks for another user
-	 * to select and preform.
-	 * 
-	 * @param context The current DSpace content
-	 * @param id The unique ID of the current workflow
-	 */
-	public static void processUnclaimTask(Context context, String id) throws SQLException, UIException, ServletException, AuthorizeException, IOException
-	{
-		WorkflowItem workflowItem = findWorkflow(context, id);
-		
-        // Return task to pool
-        WorkflowManager.unclaim(context, workflowItem, context.getCurrentUser());
-        
-        context.commit();
-        
-        //Log this unclaim action
-        log.info(LogManager.getHeader(context, "unclaim_workflow",
-                "workflow_item_id=" + workflowItem.getID() + ",item_id="
-                        + workflowItem.getItem().getID() + ",collection_id="
-                        + workflowItem.getCollection().getID() 
-                        + ",new_state=" + workflowItem.getState()));
-	}
-	
-	/**
-	 * Claim this task from the pool of unclaimed task so that this user may
-	 * preform the task by either approving or rejecting it.
-	 * 
-	 * @param context The current DSpace content
-	 * @param id The unique ID of the current workflow
-	 */
-	public static void processClaimTask(Context context, String id) throws SQLException, UIException, ServletException, AuthorizeException, IOException
-	{
-		WorkflowItem workflowItem = findWorkflow(context, id);
-		
-       // Claim the task
-       WorkflowManager.claim(context, workflowItem, context.getCurrentUser());
-       
-       context.commit();
-       
-       //log this claim information
-       log.info(LogManager.getHeader(context, "claim_task", "workflow_item_id="
-                   + workflowItem.getID() + "item_id=" + workflowItem.getItem().getID()
-                   + "collection_id=" + workflowItem.getCollection().getID()
-                   + "newowner_id=" + workflowItem.getOwner().getID() 
-                   + "new_state=" + workflowItem.getState()));
-	}
-	
-
-	/**
-	 * Reject the given task for the given reason. If the user did not provide
-	 * a reason then an error is generated placing that field in error.
-	 * 
-	 * @param context The current DSpace content
-	 * @param id The unique ID of the current workflow
-     * @param request The current request object
-	 */
-	public static String processRejectTask(Context context, String id,Request request) throws SQLException, UIException, ServletException, AuthorizeException, IOException
-	{
-		WorkflowItem workflowItem = findWorkflow(context, id);
-		
-		String reason = request.getParameter("reason");
-
-		if (reason != null && reason.length() > 1)
-		{
-            WorkspaceItem wsi = WorkflowManager.reject(context, workflowItem,context.getCurrentUser(), reason);
-			
-			//Load the Submission Process for the collection this WSI is associated with
-            Collection c = wsi.getCollection();
-            SubmissionConfigReader subConfigReader = new SubmissionConfigReader();
-            SubmissionConfig subConfig = subConfigReader.getSubmissionConfig(c.getHandle(), false);
-            
-            // Set the "stage_reached" column on the workspace item
-            // to the LAST page of the LAST step in the submission process 
-            // (i.e. the page just before "Complete", which is at NumSteps-1)
-            int lastStep = subConfig.getNumberOfSteps()-2;
-            wsi.setStageReached(lastStep);
-            wsi.setPageReached(AbstractProcessingStep.LAST_PAGE_REACHED);
-            wsi.update();
-            
-            context.commit();
-            
-            //Submission rejected.  Log this information
-            log.info(LogManager.getHeader(context, "reject_workflow", "workflow_item_id="
-                    + wsi.getID() + "item_id=" + wsi.getItem().getID()
-                    + "collection_id=" + wsi.getCollection().getID() 
-                    + "eperson_id=" + context.getCurrentUser().getID()));
-			
-			// Return no errors.
-			return null;
-		}
-		else
-		{
-			// If the user did not supply a reason then
-			// place the reason field in error.
-			return "reason";
-		}
-	}
-	
 
 	/**
 	 * Return the HTML / DRI field name for the given input.
